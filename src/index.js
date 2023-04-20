c51c1f4dd4a37c70a6972b84c3a073adf79bc2f6
prudence <prudence.jerono@student.moringaschool.com>
2023-04-20 07:32:18 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,27 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+function ColorList() {
+    const colors = [
+      "firebrick",
+      "rebeccapurple",
+      "salmon",
+      "darkslategray",
+      "hotpink",
+    ];
+  
+    const colorElements = colors.map((color, index) => (
+      <li key={index} style={{ color: color }}>{color}</li>
+    ));
+  
+    return (
+      <div>
+        <h1>Top 5 CSS Colors</h1>
+        <ol>
+          {colorElements}
+        </ol>
+      </div>
+    );
+  }
+  
